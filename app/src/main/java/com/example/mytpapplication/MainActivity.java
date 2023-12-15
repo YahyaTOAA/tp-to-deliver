adb25ea77646e0acae2d6bf6b3fac64db5ae37ce
YahyaTOAA <yahyaelomari053@gmail.com>
2023-12-14 19:26:15 -0800

    first commit

---
+++ app/src/main/java/com/example/mytpapplication/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.mytpapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.mytpapplication.R;
+
+public class MainActivity extends AppCompatActivity {
+    EditText ed1,ed2;
+    TextView tv;
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "OnCreate", Toast.LENGTH_SHORT).show();
+        ed1 = findViewById(R.id.edittext1);
+        ed2 = findViewById(R.id.edittext2);
+        btn = findViewById(R.id.button);
+        tv = findViewById(R.id.textview1);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "onRestart", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show();
+
+    }
+}
